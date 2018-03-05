149afd4c203cb0e7cf3401b894712f24f08dff80
Landeryou <m_landeryou@tss-hr420-dm35.local>
2018-03-05 13:35:01 -0500

    updates

--- public/javascripts/cmsHandler.js
+++ public/javascripts/cmsHandler.js
@@ -2,10 +2,41 @@
   //cms handler stub
   console.log('cms handler loaded');
 
-  let submitButton = document.querySelector('.add-data');
-    function addRecord() {
+  let submitButton = document.querySelector('.add-data'),
+      targetForm = document.querySelector('form');
+
+    function addRecord(e) {
       event.preventDefault(); //kill the default submit action
-      debugger;
+      //debugger;
+
+      var formData = new FormData(targetForm);
+          strData = {};
+
+      for  (var [key, value] of formData.entries()) {
+        console.log(key, value);
+
+        strData[key] = value;
+
+        //run a fetch and do a post
+        let url ="api/";
+
+        fetch(url, {
+          method : 'post',
+          headers : {
+            'Accept' : 'application/json, text-plain, */*',
+            'Content-type' : 'application/json'
+          },
+
+          body : JSON.stringify(srtData);
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+      }
     }
     submitButton.addEventListener('click', addRecord);
 })();
