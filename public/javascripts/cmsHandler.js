16dd2e47741587a44b3bb0a6e8240b9ea913979a
Landeryou <m_landeryou@tss-hr420-dm35.local>
2018-03-05 12:48:51 -0500

    updated files

--- public/javascripts/cmsHandler.js
+++ public/javascripts/cmsHandler.js
@@ -1,4 +1,11 @@
 (() => {
   //cms handler stub
   console.log('cms handler loaded');
-});
+
+  let submitButton = document.querySelector('.add-data');
+    function addRecord() {
+      event.preventDefault(); //kill the default submit action
+      debugger;
+    }
+    submitButton.addEventListener('click', addRecord);
+})();
